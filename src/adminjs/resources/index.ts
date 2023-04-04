764b21c51a231c7b53ceaac4bdcec59b1ce7fe64
ThomGomes <math.011@hotmail.com>
2023-04-04 11:36:21 -0300

    Upload de arquivos no AdminJs

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -2,6 +2,9 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
+import { Episode } from "../../models/Episode";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
+import { features } from "process";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
@@ -11,5 +14,10 @@ export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Course,
     options: courseResourceOptions
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
   }
 ]
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,13 @@
 import { Category } from './Category'
-import { Course } from './courses'
+import { Course } from './Courses'
+import { Episode } from './Episode'
 
 Category.hasMany(Course)
 
 Course.belongsTo(Category)
+Course.hasMany(Episode)
+
+Episode.belongsTo(Course)
 
 export {
   Category,
