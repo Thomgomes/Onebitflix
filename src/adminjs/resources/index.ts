cf99c74ddf4c1a15702e417889b394f32bd813d3
ThomGomes <math.011@hotmail.com>
2023-04-08 11:04:10 -0300

    Tabela de usuarios

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,9 +1,10 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course } from "../../models";
+import { Category, Course, User } from "../../models";
 import { categoryResourceOptions } from "./category";
-import { courseResourceOptions } from "./course";
 import { Episode } from "../../models/Episode";
+import { courseResourceFeatures, courseResourceOptions } from "./course";
 import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
+import { userResourceOptions } from "./user";
 import { features } from "process";
 
 export const adminJsResources: ResourceWithOptions[] = [
@@ -12,8 +13,13 @@ export const adminJsResources: ResourceWithOptions[] = [
     options: categoryResourceOptions
   },
   {
+    resource: User,
+    options: userResourceOptions
+  },
+  {
     resource: Course,
-    options: courseResourceOptions
+    options: courseResourceOptions,
+    features: courseResourceFeatures
   },
   {
     resource: Episode,
